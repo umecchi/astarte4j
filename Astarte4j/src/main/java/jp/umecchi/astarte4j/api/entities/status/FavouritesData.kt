6a46b46348ad5aed3e3aa53f4969444711de8dcd
umecchi <umecchl@yahoo.co.jp>
2022-03-20 00:31:17 +0900

    fix

--- Astarte4j/src/main/java/jp/umecchi/astarte4j/api/entities/status/FavouritesData.kt
+++ Astarte4j/src/main/java/jp/umecchi/astarte4j/api/entities/status/FavouritesData.kt
@@ -1,14 +1,11 @@
 package jp.umecchi.astarte4j.api.entities.status
 
 import com.google.gson.annotations.SerializedName
-import jp.umecchi.astarte4j.api.entities.user.Account
-import java.util.*
-
 class FavouritesData(
     @SerializedName("favourites_id")
     val favourites_id: String,
 
-    @SerializedName("statusData")
+    @SerializedName("status_data")
     val status_data: Data,
 ) {
 }
