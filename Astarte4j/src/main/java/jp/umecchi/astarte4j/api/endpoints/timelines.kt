6dfec0252b11fea7eecaa0c823323223bfd472e6
umecchi <umecchl@yahoo.co.jp>
2023-08-12 21:59:30 +0900

    HomeTimeLine

--- Astarte4j/src/main/java/jp/umecchi/astarte4j/api/endpoints/timelines.kt
+++ Astarte4j/src/main/java/jp/umecchi/astarte4j/api/endpoints/timelines.kt
@@ -28,4 +28,26 @@ class timelines(private val client: AstarteClient) {
             }
         )
     }
+
+    @Throws(AstarteRequestException::class)
+    fun getHome(
+        newest_id: String?,
+        oldest_id: String?
+    ): AstarteRequest<Status> {
+        return AstarteRequest(
+            {
+                val base_path = "timelines/home"
+                if (newest_id != null) {
+                    client.get("$base_path?newest_id=$newest_id")
+                } else if (oldest_id != null) {
+                    client.get("$base_path?oldest_id=$oldest_id")
+                } else {
+                    client.get(base_path)
+                }
+            },
+            {
+                client.getSerializer().fromJson(it, Status::class.java)
+            }
+        )
+    }
 }
